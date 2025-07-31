cc3c0b3695aad2cde1cac75ac33362b556fc79dd
MaloWinrhy <malo@arkada.fr>
2025-07-31 23:04:46 +0200

    feat: update Cargo.lock and .gitignore for improved dependency management

--- target/debug/deps/core-d47c26a92a337c69.d
+++ target/debug/deps/core-d47c26a92a337c69.d
@@ -1,5 +1,7 @@
-D:\dev\learning\openstudio\target\debug\deps\core-d47c26a92a337c69.d: crates\core\src\lib.rs
+D:\dev\learning\openstudio\target\debug\deps\core-d47c26a92a337c69.d: crates\core\src\lib.rs crates\core\src\models\mod.rs crates\core\src\models\project.rs
 
-D:\dev\learning\openstudio\target\debug\deps\libcore-d47c26a92a337c69.rmeta: crates\core\src\lib.rs
+D:\dev\learning\openstudio\target\debug\deps\libcore-d47c26a92a337c69.rmeta: crates\core\src\lib.rs crates\core\src\models\mod.rs crates\core\src\models\project.rs
 
 crates\core\src\lib.rs:
+crates\core\src\models\mod.rs:
+crates\core\src\models\project.rs:
