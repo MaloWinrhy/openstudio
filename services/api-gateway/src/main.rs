fc1e0321da2c1644f7f64fa210c28c059c63fadf
MaloWinrhy <malo@arkada.fr>
2025-08-01 00:34:30 +0200

    update api-gateway main.rs

--- services/api-gateway/src/main.rs
+++ services/api-gateway/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::post};
+use std::sync::Arc;
+use tokio::net::TcpListener;
+mod routes;
+use core::repositories::in_memory::InMemoryProjectRepo;
+
+#[tokio::main]
+async fn main() {
+    let repo = Arc::new(InMemoryProjectRepo::default());
+
+    let api_routes = routes::project::project_routes(repo);
+
+let app = Router::new()
+    .merge(api_routes);
+
+
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    println!("🚀 API running at http://127.0.0.1:3000");
+
+    axum::serve(listener, app).await.unwrap();
 }
